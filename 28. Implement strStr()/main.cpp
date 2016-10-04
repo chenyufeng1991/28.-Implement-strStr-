fdd017fc48c3b16d28a827531933cac68d6af99f
陈宇峰 <yufengcode@gmail.com>
2016-10-04 16:34:20 +0800

    1

--- 28. Implement strStr()/main.cpp
+++ 28. Implement strStr()/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int strStr(string haystack, string needle)
+{
+    long needleLength = needle.length();
+    long haystackLength = haystack.length();
+    for (long i = 0; i <= haystackLength - needleLength; i++)
+    {
+        string temp = haystack.substr(i,needleLength);
+        if (temp == needle)
+        {
+            return (int)i;
+        }
+    }
+
+    return -1;
+}
+
+int main(int argc, const char * argv[])
+{
+    int result = strStr("", "a");
+    cout << result;
     return 0;
 }
